4b468ba98d0f9e019bd9a8df6709adad5bc67811
tchisholm-DTI <68577478+tchisholm-DTI@users.noreply.github.com>
2024-10-16 20:30:28 +1300

    Working string checker (y/n and r/p/s)

---
+++ C_02_String_checker_v3.py
@@ -0,0 +1,37 @@
+# Check that users have entered a valid
+# option based on a list
+def string_checker(question, valid_ans=["yes", "no"]):
+
+    error = f"Please enter a valid option from the following list: {valid_ans}"
+
+    while True:
+
+        # Get user response and make sure it's lowercase
+        user_response = input(question).lower()
+
+        for item in valid_ans:
+            # Check if the user response is a word in the list
+            if item == user_response:
+                return item
+
+            # Check of the user response is the same as
+            # the first letter of an item in the list
+            elif user_response == item[0]:
+                return item
+
+        # Print error if user does not enter something that is valid
+        print(error)
+        print()
+
+
+# Main routine goes here
+
+rps_list = ["rock", "paper", "scissors", "xxx"]
+
+want_instructions = string_checker("Do you want to see the instructions? ")
+
+
+print("You chose: ", want_instructions)
+
+user_choice = string_checker("Choose: ", rps_list)
+print("You chose: ", user_choice)
